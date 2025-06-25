6358dcbc05098437ba77c310f2303ea808b36e74
Lachlan <lachikent@gmail.com>
2025-06-25 22:02:25 +1000

    update card sizing and padding

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -18,7 +18,7 @@ function roomCard({ Id, Name, Colour }: Room) {
   return (
     <div>
       <Link to={`/rooms/${Id}`}>
-        <div className="text-md text-center text-gray-300 font-bold shadow sm:rounded-lg border-2 max-w-50 h-22 place-content-center text-wrap transform hover:scale-115"
+        <div className="text-md text-center text-gray-300 font-bold shadow sm:rounded-lg border-2 w-35 h-35 place-content-center text-wrap transform hover:scale-115"
           style={{ borderColor: bc }}
         >
           <h3 className="p-3">{Name}</h3>
--- frontend/src/components/ColouredSection.tsx
+++ frontend/src/components/ColouredSection.tsx
@@ -12,7 +12,7 @@ export default function ColouredSection({ colour, rooms }: SecitonProps) {
   return (
     <div className="mt-5">
       <hr style={{ borderColor: bgColour }} />
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
         {rooms.map((room, id) => (
           <div className="card" key={id}>
             <Card room={room} />
