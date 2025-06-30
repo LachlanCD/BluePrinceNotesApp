a10d2318f838feb3f05b728dbc4216aefde81499
Lachlan <lachikent@gmail.com>
2025-06-30 16:25:36 +1000

    update card link to split room and general

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -4,9 +4,10 @@ import { type Card } from "../types";
 
 export default function BaseCard({ Id, Name, Colour = "white" }: Card) {
   const bc = GetHexCode(Colour);
+  const link = getLink(Colour, Id);
   return (
     <div>
-      <Link to={`/rooms/${Id}`}>
+      <Link to={link}>
         <div className="text-md text-center text-gray-300 font-bold shadow sm:rounded-lg border-2 w-35 h-35 place-content-center text-wrap transform hover:scale-115"
           style={{ borderColor: bc }}
         >
@@ -16,3 +17,8 @@ export default function BaseCard({ Id, Name, Colour = "white" }: Card) {
     </div>
   );
 };
+
+function getLink(colour: string, id: number) {
+  if (colour === "white") return `/general/${id}`
+  return`/room/${id}` 
+}
